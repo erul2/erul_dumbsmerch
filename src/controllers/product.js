aad75edac7dd2deef28f0e3af352f651f52eaafc
erul2 <erulhaerulloh2@gmail.com>
2022-01-05 15:16:59 +0700

    update file upload

--- src/controllers/product.js
+++ src/controllers/product.js
@@ -183,11 +183,17 @@ exports.updateProduct = async (req, res) => {
     let { categoryId } = req.body;
     categoryId = await categoryId.split(",");
 
+    const result = await cloudinary.uploader.upload(req.file.path, {
+      folder: "dumbmerch_file",
+      use_filename: true,
+      unique_filename: false,
+    });
+
     const data = {
       name: req?.body?.name,
       desc: req?.body.desc,
       price: req?.body?.price,
-      image: req?.file?.filename,
+      image: result.public_id,
       qty: req?.body?.qty,
       idUser: req?.user?.id,
     };
